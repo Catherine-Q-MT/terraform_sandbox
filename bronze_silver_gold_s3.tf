f7cc7b0ff9db7f654b080e39b747b60dbddab6f5
Catherine Quilley <catherine.quilley@madetech.com>
2023-05-25 17:51:12 +0100

    directory shuffle

--- bronze_silver_gold_s3.tf
+++ bronze_silver_gold_s3.tf
@@ -1,12 +1,12 @@
 
 
 resource "aws_s3_bucket" "dataBuckets" {
-  count = length(var.bucket_list)
+  count  = length(var.bucket_list)
   bucket = "${var.bucket_prefix}${var.bucket_list[count.index]}"
 }
 
 resource "aws_s3_bucket_ownership_controls" "dataBuckets" {
-  count = length(var.bucket_list)
+  count  = length(var.bucket_list)
   bucket = aws_s3_bucket.dataBuckets[count.index].bucket
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_ownership_controls" "dataBuckets" {
 }
 
 resource "aws_s3_bucket_acl" "dataBuckets" {
-  count = length(var.bucket_list)
+  count      = length(var.bucket_list)
   depends_on = [aws_s3_bucket_ownership_controls.dataBuckets]
 
   bucket = aws_s3_bucket.dataBuckets[count.index].bucket
